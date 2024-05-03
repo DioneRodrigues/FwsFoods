3303274819be5f0579922d9cb0fc2f38800955f3
Dione Rodrigues <dione.kacoal@gmail.com>
2024-05-02 23:08:03 -0300

    feat: add name restaurant product item

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,5 +1,7 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
+import { calculeteProductTotalPrice, formatCurrency } from "../_helpers/price";
+import { ArrowDownIcon } from "lucide-react";
 
 interface ProductItemProps {
   product: Product;
@@ -7,7 +9,7 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="w-[150px] min-w-[150px] space-y-8">
+    <div className="w-[150px] min-w-[150px] space-y-4">
       {/* div da renderização da imagem */}
       <div className="relative h-[150px] w-full">
         <Image
@@ -16,19 +18,33 @@ const ProductItem = ({ product }: ProductItemProps) => {
           fill
           className="rounded-lg object-cover shadow-md"
         />
+
+        {product.discountPercentage && (
+          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-white">
+            <ArrowDownIcon size={12} />
+            <span className="text-xs font-semibold">
+              {product.discountPercentage}%
+            </span>
+          </div>
+        )}
       </div>
 
       {/* div da renderização dos dados do produto */}
       <div>
-        <h2>{product.name}</h2>
+        <h2 className="truncate text-sm">{product.name}</h2>
 
-        <div>
-          {Intl.NumberFormat("pt-br", {
-            style: "currency",
-            currency: "BRL",
-            minimumFractionDigits: 2,
-          }).format(Number(product.price))}
+        <div className="flex items-center gap-1">
+          <h3 className="font-semibold ">
+            {formatCurrency(calculeteProductTotalPrice(product))}
+          </h3>
+          {product.discountPercentage > 0 && (
+            <span className="text-xs text-muted-foreground line-through">
+              {formatCurrency(Number(product.price))}
+            </span>
+          )}
         </div>
+
+        <span>{}</span>
       </div>
     </div>
   );
